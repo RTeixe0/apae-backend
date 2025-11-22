145b1109928ddaab6a2c57579aaa97f401320193
RTeixe0 <renanteixeira121@outlook.com>
2025-11-22 14:36:47 -0300

    seed

---
+++ test_mysql.js
@@ -0,0 +1,15 @@
+import mysql from "mysql2/promise";
+
+const db = await mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME
+});
+
+try {
+  const [rows] = await db.query("SELECT NOW() AS current_time");
+  console.log("OK:", rows);
+} catch (err) {
+  console.log("ERRO:", err);
+}
